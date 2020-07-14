31355c0cc9cb61e54327124cea046fcc6e0e9756
MohdZyd <68237076+MohdZyd@users.noreply.github.com>
2020-07-14 11:37:22 +0300

    Add files via upload

---
+++ 2Assigmnet.js
@@ -0,0 +1,23 @@
+
+var today = new Date();
+var dd = today.getDate();
+
+var mm = today.getMonth()+1; 
+var yyyy = today.getFullYear();
+if(dd<10) 
+{
+    dd='0'+dd;
+} 
+
+if(mm<10) 
+{
+    mm='0'+mm;
+} 
+today = mm+'-'+dd+'-'+yyyy;
+document.writeln(today);
+today = mm+'/'+dd+'/'+yyyy;
+document.writeln(today);
+today = dd+'-'+mm+'-'+yyyy;
+document.writeln(today);
+today = dd+'/'+mm+'/'+yyyy;
+document.writeln(today);
